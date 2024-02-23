595e6fffff1f0176b2657cf3133c08b6e93cd3a8
Lalatianalinda <lalatianalinda@gmail.com>
2024-02-23 16:04:21 +0100

    Ajout du convertisseur txt

---
+++ sprint3/FileTXT.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 14:53:52 2024
+
+@author: lalatiana
+"""
+
+import os
+from prototype import Parser
+
+class FileTXT:
+    def __init__(self):
+        self.title = ""
+        self.authors = ""
+        self.abstract = ""
+        self.parser = Parser()
+        
+    """
+        Fonction qui Supprime Abstract s'il revient deux fois
+    """    
+    def find_abstractTXT(self,path) :        
+        resultat, pos = self.parser.find_abstract(path)
+        self.abstract = resultat.replace("Abstract", "")    
+        return [self.abstract.strip(), pos]
+    
+    """
+        Fonction qui traite les valeurs d'un tableau de auteurs 
+    """  
+    def getAuthorTXT(self,path):
+        auteurs = self.parser.getAuthor(path)
+        val=""
+        if isinstance(auteurs, list) and auteurs:
+            for auteur in auteurs:
+                val+=auteur+" "
+            self.authors = val
+            return self.authors
+        else :
+            self.authors = auteurs
+            return self.authors
+     
+     
+    """
+        Fonction qui trouve le titre 
+    """  
+    def find_titleTXT(self,path) :
+        self.title = self.parser.find_title(path)
+        return self.title
+    
+    """
+        Ecris le fichier TXT  
+    """    
+    def write_file(fileTXT, src, dst):
+    
+            if not os.path.exists(dst):
+                os.makedirs(dst)
+            for fichier in os.listdir(src) :
+                sfile = os.path.join(src,fichier)
+                #Obtient le nom du fichier sans l'extension
+                base_name = os.path.splitext(fichier)[0]
+                dfile = os.path.join(dst, base_name + '.txt')
+                with open(dfile,'w') as d :
+                    d.write("Nom fichier source : ")
+                    d.write(base_name+".pdf")
+                    d.write("\n_____________________________\n")
+                    d.write("Titres : ")
+                    d.write(fileTXT.find_titleTXT(sfile))
+                    d.write("\n_____________________________\n")
+                    d.write("Auteurs : ")
+                    d.write(str(fileTXT.getAuthorTXT(sfile)))
+                    d.write("\n_____________________________\n")
+                    d.write("Abstract : ")
+                    d.write(str(fileTXT.find_abstractTXT(sfile)[0]))
+                    
+if __name__ == "__main__" :
+    fileTXT = FileTXT()
+    src = "../corpus/pdf/"
+    dst = "../sprint3/resultatsTXT"
+    fileTXT.write_file(src,dst)
+    print("réussi")
